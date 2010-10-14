7dafab8a439968bbaafffffb0404059d43d458d2
Kris Zyp <kriszyp@gmail.com>
2010-10-14 09:17:30 -0600

    added define to global leak test

--- test/simple/test-global-leak.js
+++ test/simple/test-global-leak.js
@@ -7,6 +7,7 @@ var knownGlobals = [ setTimeout
                    , console
                    , Buffer
                    , process
+				   , define
                    , global
                    ];
 
