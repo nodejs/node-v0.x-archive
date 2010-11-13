4d57ba7617ef1b979099a8e196200a51bbf020cb
Daniel Gröber <darklord@darkboxed.org>
2010-11-13 02:04:33 +0100

    Fixed typo

--- cmake/v8_build.cmake
+++ cmake/v8_build.cmake
@@ -35,7 +35,8 @@ if(NOT SHARED_V8)
     PROPERTY IMPORTED_LOCATION ${PROJECT_BINARY_DIR}/deps/v8/${v8_fn})
 
 
-  if(CAMKE_VERSION VERSION_GREATER 2.8)
+  
+  if(CMAKE_VERSION VERSION_GREATER 2.8)
     # use ExternalProject for CMake >2.8
     include(ExternalProject)
     
