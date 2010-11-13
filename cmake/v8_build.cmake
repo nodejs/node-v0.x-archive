64a60d289a6f20ec791c03c1f1e8a4a619ee9496
Daniel Gröber <darklord@darkboxed.org>
2010-11-13 01:19:28 +0100

    restored compatibility with CMake 2.6

--- cmake/v8_build.cmake
+++ cmake/v8_build.cmake
@@ -1,7 +1,6 @@
 #
 # v8 build stuff
 #
-include(ExternalProject)
 
 string(TOLOWER ${CMAKE_BUILD_TYPE} v8mode)
 set(v8arch ${node_arch})
@@ -25,28 +24,64 @@ if(NOT SHARED_V8)
       OUTPUT_VARIABLE cpu_count)
   endif()
 
-  if(${cpu_count} GREATER 1 AND NOT _ignore_cpuc)
+  if(${cpu_count} GREATER 1)
     math(EXPR parallel_jobs ${cpu_count}*2)
   else()
     set(parallel_jobs 1)
   endif()
 
-  ExternalProject_Add(v8_extprj
-    URL ${PROJECT_SOURCE_DIR}/deps/v8
-
-    BUILD_IN_SOURCE True
-    BUILD_COMMAND sh -c "${PROJECT_BINARY_DIR}/tools/scons/scons.py library=static visibility=default ${v8snapshot} mode=${v8mode} verbose=on arch=${v8arch} -j ${parallel_jobs}"
-
-    SOURCE_DIR ${PROJECT_BINARY_DIR}/deps/v8
-    # ignore this stuff, it's not needed for building v8 but ExternalProject
-    # demands these steps
-
-    CONFIGURE_COMMAND "true" # fake configure
-    INSTALL_COMMAND "true" # fake install
-    )
-
   add_library(v8 STATIC IMPORTED)
-  add_dependencies(node v8_extprj)
   set_property(TARGET v8
     PROPERTY IMPORTED_LOCATION ${PROJECT_BINARY_DIR}/deps/v8/${v8_fn})
+
+
+  if(CAMKE_VERSION VERSION_GREATER 2.8)
+    # use ExternalProject for CMake >2.8
+    include(ExternalProject)
+    
+    ExternalProject_Add(v8_extprj
+      URL ${PROJECT_SOURCE_DIR}/deps/v8
+      
+      BUILD_IN_SOURCE True
+      BUILD_COMMAND sh -c "${PROJECT_BINARY_DIR}/tools/scons/scons.py library=static visibility=default ${v8snapshot} mode=${v8mode} verbose=on arch=${v8arch} -j ${parallel_jobs}"
+      
+      SOURCE_DIR ${PROJECT_BINARY_DIR}/deps/v8
+      # ignore this stuff, it's not needed for building v8 but ExternalProject
+      # demands these steps
+      
+      CONFIGURE_COMMAND "true" # fake configure
+      INSTALL_COMMAND "true" # fake install
+      )
+    
+    add_dependencies(node v8_extprj)
+  else(CAMKE_VERSION VERSION_GREATER 2.8)
+    # copy v8 sources inefficiently with CMake versions <2.8
+    file(GLOB_RECURSE v8_sources RELATIVE ${PROJECT_SOURCE_DIR} deps/v8/*)
+    
+    if(NOT ${in_source_build})
+      file(MAKE_DIRECTORY ${PROJECT_BINARY_DIR}/deps/v8)
+      
+      foreach(FILE ${v8_sources})
+        add_custom_command(OUTPUT ${PROJECT_BINARY_DIR}/${FILE}
+          COMMAND ${CMAKE_COMMAND} -E copy_if_different ${PROJECT_SOURCE_DIR}/${FILE} ${PROJECT_BINARY_DIR}/${FILE}
+          DEPENDS ${PROJECT_SOURCE_DIR}/${FILE}
+          )
+        list(APPEND v8_sources_dest ${PROJECT_BINARY_DIR}/${FILE})
+      endforeach()
+    else()
+      set(v8_sources_dest ${v8_sources})
+    endif()
+
+    add_custom_command(
+      OUTPUT ${PROJECT_BINARY_DIR}/deps/v8/${v8_fn}
+      COMMAND ${PROJECT_BINARY_DIR}/tools/scons/scons.py library=static visibility=default ${v8snapshot} mode=${v8mode} verbose=on arch=${v8arch} -j ${parallel_jobs}
+      WORKING_DIRECTORY ${PROJECT_BINARY_DIR}/deps/v8/
+      DEPENDS ${v8_sources_dest}
+    )
+
+    add_custom_target(v8_stock ALL DEPENDS ${PROJECT_BINARY_DIR}/deps/v8/${v8_fn})
+    set_property(TARGET v8 PROPERTY
+      IMPORTED_LOCATION ${PROJECT_BINARY_DIR}/deps/v8/${v8_fn})
+    add_dependencies(node v8_stock)
+  endif()
 endif()
