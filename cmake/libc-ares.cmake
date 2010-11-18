425b0ebcb7e4715089d866ce7747c94c1d26f283
Tom Hughes <tom.hughes@palm.com>
2010-11-18 00:14:26 -0600

    Fix cmake c-ares architecture detection.

--- cmake/libc-ares.cmake
+++ cmake/libc-ares.cmake
@@ -5,6 +5,14 @@ if(SHARED_CARES)
     ) # Find header
   find_package_handle_standard_args(libcares DEFAULT_MSG LIBCARES_LIBRARY LIBCARES_INCLUDE_DIR)
 else()
+  set(cares_arch ${node_arch})
+
+  if(${node_arch} MATCHES x86_64)
+    set(cares_arch x64)
+  elseif(${node_arch} MATCHES x86)
+    set(cares_arch ia32)
+  endif()
+
   add_subdirectory(deps/c-ares)
-  set(LIBCARES_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/deps/c-ares ${CMAKE_SOURCE_DIR}/deps/c-ares/${node_platform}-${node_arch})
+  set(LIBCARES_INCLUDE_DIR ${CMAKE_SOURCE_DIR}/deps/c-ares ${CMAKE_SOURCE_DIR}/deps/c-ares/${node_platform}-${cares_arch})
 endif()
