958a4c6ba7ffaf0bb910a006c5d552a62e9d1ef5
Anders Conbere <aconbere@gmail.com>
2010-11-25 12:51:51 -0800

    clean up module contextLoad assignment

--- src/node.js
+++ src/node.js
@@ -94,9 +94,7 @@ var module = (function () {
   var exports = {};
   // Set the environ variable NODE_MODULE_CONTEXTS=1 to make node load all
   // modules in thier own context.
-  var contextLoad = false;
-  if (+process.env["NODE_MODULE_CONTEXTS"] > 0) contextLoad = true;
-  var Script;
+  var contextLoad = !!parseInt(process.env.NODE_MODULE_CONTEXTS);
 
   var moduleCache = {};
 
