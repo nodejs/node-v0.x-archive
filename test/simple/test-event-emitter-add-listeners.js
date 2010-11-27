20fcdf1ddb8404ee9acd4cdf53a9ad6248ecec2a
Fedor Indutny <fedor.indutny@gmail.com>
2010-11-27 15:21:16 +0600

    events.js patch, adds event propagation and few constants. Thx @isaacs

--- test/simple/test-event-emitter-add-listeners.js
+++ test/simple/test-event-emitter-add-listeners.js
@@ -21,11 +21,28 @@ e.on("hello", function (a, b) {
 
 console.log("start");
 
-e.emit("hello", "a", "b");
+var times_prop_emited = 0;
+
+function onprop() {
+  times_prop_emited++;
+  return false;
+}
+e.on("prop", onprop);
+e.on("prop", onprop);
+
+var result_of_not_stopped = e.emit("hello", "a", "b");
+
+var result_of_not_called = e.emit("does-not-exist");
+
+var result_of_stopped = e.emit("prop");
 
 process.addListener("exit", function () {
-  assert.deepEqual(["hello"], events_new_listener_emited);
+  assert.deepEqual(["hello", "prop", "prop"], events_new_listener_emited);
   assert.equal(1, times_hello_emited);
+  assert.equal(1, times_prop_emited);
+  assert.equal(result_of_not_stopped, events.EVENT_EMITTED);
+  assert.equal(result_of_not_called, events.NO_EFFECT);
+  assert.equal(result_of_stopped, events.STOPPED_PROPAGATION);
 });
 
 
