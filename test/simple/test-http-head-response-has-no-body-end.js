776c25c4392dfb076a9fc0e85a2145910194df2c
Reid Burke <me@reidburke.com>
2011-07-07 21:24:27 -0700

    Correct quote style for test.

--- test/simple/test-http-head-response-has-no-body-end.js
+++ test/simple/test-http-head-response-has-no-body-end.js
@@ -30,7 +30,7 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
   res.writeHead(200);
-  res.end("FAIL"); // broken: sends FAIL from hot path.
+  res.end('FAIL'); // broken: sends FAIL from hot path.
 });
 server.listen(common.PORT);
 
