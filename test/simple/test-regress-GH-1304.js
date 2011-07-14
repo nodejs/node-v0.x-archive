e25f41746878b359fe869aa112a87ac230063789
Stefan Rusu <saltwaterc@gmail.com>
2011-07-14 17:26:25 +0300

    Adds a regression test for #1304.

---
+++ test/simple/test-regress-GH-1304.js
@@ -0,0 +1,19 @@
+// Regression test for GH-1304
+// https://github.com/joyent/node/issues/1304
+//
+// This test works by creating a HTTPS connection to a server, then aborting
+// it. The purpose of this test is to have a tls module thou shalt not fail if
+// the request is aborted.
+
+var common = require('../common');
+var https = require('https');
+
+var opt = {
+	host: 'encrypted.google.com',
+	path: '/'
+};
+
+var req = https.get(opt, function (res) {
+});
+
+req.abort();
