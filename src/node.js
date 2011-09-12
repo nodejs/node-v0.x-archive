aac65eda66e1c309656cbe0b434ba4bfd2c28683
Aaron Stacy <aaron.r.stacy@gmail.com>
2011-09-12 15:43:25 -0500

    REPL sources file from NODE_STARTUP env var

--- src/node.js
+++ src/node.js
@@ -107,8 +107,26 @@
       var Module = NativeModule.require('module');
 
       if (NativeModule.require('tty').isatty(fd)) {
+
         // REPL
-        Module.requireRepl().start();
+        var ctx = Module.requireRepl().start().context;
+
+        var startup = process.env.NODE_STARTUP;
+        if (startup) {
+          var path = NativeModule.require('path');
+          var fs = NativeModule.require('fs');
+          var vm = NativeModule.require('vm');
+
+          path.exists(startup, function(exists) {
+            if (exists) {
+              fs.readFile(startup, function(err, data) {
+                if (! err) {
+                  vm.runInContext(data.toString('utf8'), ctx);
+                }
+              });
+            }
+          });
+        }
 
       } else {
         // Read all of stdin - execute it.
