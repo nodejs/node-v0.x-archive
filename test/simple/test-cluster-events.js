50820e73e7bf9352b38cf8948aba7c7b5ec5dc78
Andreas Madsen <amwebdk@gmail.com>
2011-11-07 16:02:50 +0100

    added testcase for cluster events

---
+++ test/simple/test-cluster-events.js
@@ -0,0 +1,43 @@
+var cluster = require("cluster");
+var common = require('../common');
+var assert = require('assert');
+var cpus = require("os").cpus().length;
+
+if (cluster.isMaster) {
+	
+	var fork = false, online = false, listening = false;
+	
+	//Create cluster
+	cluster
+		.on('fork', function (worker) {
+			fork = true;
+		})
+		.on("online", function (worker) {
+			online = true;
+		})
+		.on("listening", function (worker, adress) {
+			listening = true;
+			process.exit();
+		})
+		.setupMaster({
+			workers : 1
+		})
+		.autoFork();
+	
+	global.setTimeout(function () {
+		process.exit();
+		assert.fail("Timeout after 2 seconds");
+	}, 2000);
+	
+	process.on("exit", function () {
+		assert.ok(fork, "The fork event didn't emit");
+		assert.ok(online, "The online event didn't emit");
+		assert.ok(listening, "The listening event didn't emit");
+	});
+	
+} else if (cluster.isWorker) {
+
+	require("http")
+		.createServer()
+		.listen(common.PORT, "127.0.0.1");
+}
