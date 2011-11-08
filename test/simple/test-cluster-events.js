4f9c93856b9ac771fc11cd63f88ac578b4c977d7
Andreas Madsen <amwebdk@gmail.com>
2011-11-08 21:25:35 +0100

    fit test after removing chain

--- test/simple/test-cluster-events.js
+++ test/simple/test-cluster-events.js
@@ -1,42 +1,43 @@
-var cluster = require("cluster");
+var cluster = require('cluster');
 var common = require('../common');
 var assert = require('assert');
 
 if (cluster.isMaster) {
-	
-	var fork = false, online = false, listening = false;
-	
-	//Create cluster
-	cluster
-		.on('fork', function (worker) {
-			fork = true;
-		})
-		.on("online", function (worker) {
-			online = true;
-		})
-		.on("listening", function (worker, adress) {
-			listening = true;
-			process.exit();
-		})
-		.setupMaster({
-			workers : 1
-		})
-		.autoFork();
-	
-	global.setTimeout(function () {
-    assert.fail("Timeout after 2 seconds");
-    process.exit();
-	}, 2000);
-	
-	process.on("exit", function () {
-		assert.ok(fork, "The fork event didn't emit");
-		assert.ok(online, "The online event didn't emit");
-		assert.ok(listening, "The listening event didn't emit");
-	});
-	
+
+  var fork = false;
+  var online = false;
+  var listening = false;
+
+  //Create cluster
+  cluster.on('fork', function(worker) {
+    fork = true;
+  });
+  cluster.on('online', function(worker) {
+    online = true;
+  });
+  cluster.on('listening', function(worker, adress) {
+    listening = true;
+    process.exit(0);
+  });
+  cluster.setupMaster({
+    workers: 1
+  });
+  cluster.autoFork();
+
+  global.setTimeout(function() {
+    assert.fail('Timeout after 2 seconds');
+    process.exit(1);
+  }, 2000);
+
+  process.on('exit', function() {
+    assert.ok(fork, 'The fork event did not emit');
+    assert.ok(online, 'The online event did not emit');
+    assert.ok(listening, 'The listening event did not emit');
+  });
+
 } else if (cluster.isWorker) {
 
-	require("http")
-		.createServer()
-		.listen(common.PORT, "127.0.0.1");
+  require('http')
+    .createServer()
+    .listen(common.PORT, '127.0.0.1');
 }
