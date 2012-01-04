7d316f97d5b0734bd84cd88ff8ce0f66cd5510b3
Andreas Madsen <amwebdk@gmail.com>
2012-01-04 19:03:05 +0100

    jslint

--- test/simple/test-child-process-silent.js
+++ test/simple/test-child-process-silent.js
@@ -23,60 +23,60 @@ var common = require('../common');
 var assert = require('assert');
 var childProcess = require('child_process');
 
-//Child pipe test
+// Child pipe test
 if (process.argv[2] === 'pipetest') {
   process.stdout.write('stdout message');
   process.stderr.write('stderr message');
 }
 
-//Child IPC test
+// Child IPC test
 else if (process.argv[2] === 'ipctest') {
   process.send('message from child');
-  process.on('message', function () {
+  process.on('message', function() {
     process.send('got message from master');
   });
 }
 
-//Parent | start child pipe test
+// Parent | start child pipe test
 else if (process.argv[2] === 'parent') {
 
   var child = childProcess.fork(process.argv[1], ['pipetest'], {silent: true});
 
-  //Allow child process to self terminate
+  // Allow child process to self terminate
   child._channel.close();
   child._channel = null;
 
-  child.on('exit', function () {
+  child.on('exit', function() {
     process.exit(0);
   });
 }
 
-//testcase | start parent && child IPC test
+// testcase | start parent && child IPC test
 else {
 
-  //testing: is stderr and stdout piped to parent
+  // testing: is stderr and stdout piped to parent
   var parent = childProcess.spawn(process.execPath, [process.argv[1], 'parent']);
 
   //got any stderr or std data
   var stdoutData = false;
-  parent.stdout.on('data', function () {
+  parent.stdout.on('data', function() {
     stdoutData = true;
   });
   var stderrData = false;
-  parent.stdout.on('data', function () {
+  parent.stdout.on('data', function() {
     stderrData = true;
   });
 
-  //testing: do message system work when using silent
+  // testing: do message system work when using silent
   var child = childProcess.fork(process.argv[1], ['ipctest'], {silent: true});
 
-  //Manual pipe so we will get errors
+  // Manual pipe so we will get errors
   child.stderr.pipe(process.stderr, {end: false});
   child.stdout.pipe(process.stdout, {end: false});
 
   var childSending = false;
   var childReciveing = false;
-  child.on('message', function (message) {
+  child.on('message', function(message) {
     if (childSending === false) {
       childSending = (message === 'message from child');
     }
@@ -91,7 +91,7 @@ else {
   });
   child.send('message to child');
 
-  //Check all values
+  // Check all values
   process.on('exit', function() {
     //clean up
     child.kill();
