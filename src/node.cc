f703ee34fd8c0598392d19a585920d583dd1e39a
Andreas Madsen <amwebdk@gmail.com>
2012-02-02 22:00:50 +0100

    change SetErrno to use a number

--- src/node.cc
+++ src/node.cc
@@ -1010,11 +1010,9 @@ void SetErrno(uv_err_t err) {
     snprintf(errno_buf, 100, "Unknown system errno %d", err.sys_errno_);
     Context::GetCurrent()->Global()->Set(errno_symbol, String::New(errno_buf));
   } else {
-    Context::GetCurrent()->Global()->Set(errno_symbol,
-                                         String::NewSymbol(uv_err_name(err)));
+    Context::GetCurrent()->Global()->Set(errno_symbol, Number::New(err.sys_errno_));
   }
 }
-
 
 enum encoding ParseEncoding(Handle<Value> encoding_v, enum encoding _default) {
   HandleScope scope;
