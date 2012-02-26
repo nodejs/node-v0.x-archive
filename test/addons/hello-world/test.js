9ba0865879b685da452d21200d407db4fa624a75
Nathan Rajlich <nathan@tootallnate.net>
2012-02-26 14:26:33 -0800

    Revert "Require the proper .node file on Windows in the hello world addon."
    This reverts commit 63e9ad29b9dbf822f60b10eedc03fcce8880f637.
    
    Not needed because of joyent/node#2833.

--- test/addons/hello-world/test.js
+++ test/addons/hello-world/test.js
@@ -1,9 +1,4 @@
 var assert = require('assert');
-var binding;
-if (process.platform == 'win32') {
-  binding = require('./Release/binding');
-} else {
-  binding = require('./out/Release/binding');
-}
+var binding = require('./out/Release/binding');
 assert.equal('world', binding.hello());
 console.log('binding.hello() =', binding.hello());
