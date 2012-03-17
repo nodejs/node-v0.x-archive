e4d47143b6d28430a07a5c6ef71e1643d81b9a48
Thiago Arrais <thiago.arrais@gmail.com>
2012-03-16 21:22:38 -0300

    Making the linter happy

--- test/simple/test-crypto.js
+++ test/simple/test-crypto.js
@@ -417,10 +417,12 @@ var ciph = cipher.update(plaintext, 'utf8', 'hex');
 // Only use binary or hex, not base64.
 ciph += cipher.final('hex');
 
-var key_buf = new Buffer('4d795365637265744b6579313233', 'hex'); //MySecretKey123
+//MySecretKey123
+var key_buf = new Buffer('4d795365637265744b6579313233', 'hex');
 var buf_cipher = crypto.createCipher('aes192', key_buf);
 
-var buf_ciph = buf_cipher.update(plaintext, 'utf8', 'hex') + buf_cipher.final('hex');
+var buf_ciph = buf_cipher.update(plaintext, 'utf8', 'hex');
+buf_ciph += buf_cipher.final('hex');
 
 assert.equal(ciph, buf_ciph, 'key as string and buffer');
 
