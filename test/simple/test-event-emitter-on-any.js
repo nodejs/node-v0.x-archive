fe087003d0a90f987d20a611f922b3853edaa6f6
Pedro Teixeira <pedro.teixeira@gmail.com>
2012-06-06 17:15:54 +0100

    [events] EventEmitter emits wildcard event. Supports .onAny(cb) and .on('*', cb).

---
+++ test/simple/test-event-emitter-on-any.js
@@ -0,0 +1,56 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+var events = require('events');
+
+var e = new events.EventEmitter();
+
+var events_new_listener_emited = [];
+var listeners_new_listener_emited = [];
+var times_hello_emited = 0;
+
+e.on('newListener', function(event, listener) {
+  events_new_listener_emited.push(event);
+  listeners_new_listener_emited.push(listener);
+});
+
+function hello(eventType, args) {
+  console.log('hello');
+  times_hello_emited += 1;
+  assert.equal('hello', eventType);
+  assert.deepEqual(['a', 'b'], args);
+}
+e.onAny(hello);
+
+console.log('start');
+
+e.emit('hello', 'a', 'b');
+
+
+process.on('exit', function() {
+  assert.deepEqual(['*'], events_new_listener_emited);
+  assert.deepEqual([hello], listeners_new_listener_emited);
+  assert.equal(1, times_hello_emited);
+});
+
+
---
+++ test/simple/test-event-emitter-remove-any-listener.js
@@ -0,0 +1,75 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var common = require('../common');
+var assert = require('assert');
+var events = require('events');
+
+
+var count = 0;
+
+function listener1() {
+  console.log('listener1');
+  count++;
+}
+
+function listener2() {
+  console.log('listener2');
+  count++;
+}
+
+function listener3() {
+  console.log('listener3');
+  count++;
+}
+
+var e1 = new events.EventEmitter();
+var e1listeners = e1.listeners('*');
+e1.onAny(listener1);
+assert.equal(e1listeners.length, 1);
+e1.removeAnyListener(listener1);
+assert.deepEqual([], e1.listeners('*'));
+e1.emit('a');
+assert.equal(0, count);
+e1.onAny(listener1);
+e1.emit('a');
+assert.equal(1, count);
+
+// identity check, listeners array should be the same
+assert.equal(e1listeners, e1.listeners('*'));
+
+var e2 = new events.EventEmitter();
+e2.onAny(listener1);
+e2.removeAnyListener(listener2);
+assert.deepEqual([listener1], e2.listeners('*'));
+
+var e3 = new events.EventEmitter();
+e3.onAny(listener1);
+e3.onAny(listener2);
+var e3listeners = e3.listeners('*');
+assert.equal(e3listeners.length, 2)
+e3.removeAnyListener(listener1);
+assert.equal(e3listeners.length, 1)
+assert.deepEqual([listener2], e3.listeners('*'));
+
+assert.equal(e3listeners, e3.listeners('*'));
+
+
