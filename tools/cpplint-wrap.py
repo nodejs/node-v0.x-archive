d50e686ca58be7536ade3dc261ce624c90743df2
Matt Gollob <mattgollob@gmail.com>
2012-06-10 20:43:14 -0400

    Normalize whitespace in tools/cpplint-wrap.py

--- tools/cpplint-wrap.py
+++ tools/cpplint-wrap.py
@@ -7,31 +7,31 @@ cpplint = ['python', 'tools/cpplint.py']
 excludes = [
   'src{0}{1}'.format(os.sep, 'ngx-queue.h'),
   'src{0}{1}'.format(os.sep, 'node_constants.cc'),
-	'src{0}{1}'.format(os.sep, 'node_extensions.h'),
-	'src{0}{1}'.format(os.sep, 'node_root_certs.h'),
-	'src{0}{1}'.format(os.sep, 'v8_typed_array.cc'),
-	'src{0}{1}'.format(os.sep, 'v8_typed_array.h'),
-	'src{0}{1}'.format(os.sep, 'v8abbr.h')
+  'src{0}{1}'.format(os.sep, 'node_extensions.h'),
+  'src{0}{1}'.format(os.sep, 'node_root_certs.h'),
+  'src{0}{1}'.format(os.sep, 'v8_typed_array.cc'),
+  'src{0}{1}'.format(os.sep, 'v8_typed_array.h'),
+  'src{0}{1}'.format(os.sep, 'v8abbr.h')
 ]
 
 lintfiles = filter(lambda x: excludes.count(x) == 0,
-	glob.glob("src/*.cc") + glob.glob("src/*.h") + glob.glob("src/*.c"))
+  glob.glob("src/*.cc") + glob.glob("src/*.h") + glob.glob("src/*.c"))
 
 run_args = [
   ['--filter=-readability/fn_size', 'src/node_constants.cc'],
-	['--filter=-build/header_guard', 'src/node_extensions.h'],
-	['--filter=-whitespace/line_length', 'src/node_root_certs.h'],
-	['--filter=-legal/copyright',
-		'src/v8_typed_array.cc',
-		'src/v8_typed_array.h',
-		'src/v8abbr.h'
-	],
-	lintfiles # no filters for the "regular" files
+  ['--filter=-build/header_guard', 'src/node_extensions.h'],
+  ['--filter=-whitespace/line_length', 'src/node_root_certs.h'],
+  ['--filter=-legal/copyright',
+    'src/v8_typed_array.cc',
+    'src/v8_typed_array.h',
+    'src/v8abbr.h'
+  ],
+  lintfiles # no filters for the "regular" files
 ]
 
 for args in run_args:
-	exitcode = call(cpplint + args)
-	if not exitcode == 0:
-		break
+  exitcode = call(cpplint + args)
+  if not exitcode == 0:
+    break
 
 sys.exit(exitcode)
