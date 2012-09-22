cb3bcdc5c3d7019dd4d1f96573e9d9a46ba5c11e
Ben Noordhuis <info@bnoordhuis.nl>
2012-09-22 03:32:39 +0200

    test: events: check removeListener event order

--- test/simple/test-event-emitter-remove-listeners.js
+++ test/simple/test-event-emitter-remove-listeners.js
@@ -40,6 +40,14 @@ function listener3() {
   count++;
 }
 
+function remove1() {
+  assert(0);
+}
+
+function remove2() {
+  assert(0);
+}
+
 var e1 = new events.EventEmitter();
 e1.on('hello', listener1);
 e1.on('removeListener', common.mustCall(function(name, cb) {
@@ -64,3 +72,13 @@ e3.on('removeListener', common.mustCall(function(name, cb) {
 }));
 e3.removeListener('hello', listener1);
 assert.deepEqual([listener2], e3.listeners('hello'));
+
+var e4 = new events.EventEmitter();
+e4.on('removeListener', common.mustCall(function(name, cb) {
+  if (cb !== remove1) return;
+  this.removeListener('quux', remove2);
+  this.emit('quux');
+}, 2));
+e4.on('quux', remove1);
+e4.on('quux', remove2);
+e4.removeListener('quux', remove1);
