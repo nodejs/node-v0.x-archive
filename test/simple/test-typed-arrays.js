18ea925cbd5c80a903a2c0c372642db4abb5e9da
Dean McNamee <dean@gmail.com>
2012-12-19 23:43:22 +0100

    Remove TypedArrays get(). This seems to have been added as a result of misreading the spec, there is no get() method, only a getter (which the spec names get()), but this is actually the [] operator.  There are many webkit tests that explicitly test for the fact that the get() method is abscent.  Remove it to conform to the spec.

--- test/simple/test-typed-arrays.js
+++ test/simple/test-typed-arrays.js
@@ -153,13 +153,13 @@ sub[1] = 0x34;
 assert.equal(uint8[2], 0x12);
 assert.equal(uint8[3], 0x34);
 
-// test .set(index, value), .set(arr, offset) and .get(index)
+// test .set(index, value), .set(arr, offset)
 uint8.set(1, 0x09);
 uint8.set([0x0a, 0x0b], 2);
 
-assert.equal(uint8.get(1), 0x09);
-assert.equal(uint8.get(2), 0x0a);
-assert.equal(uint8.get(3), 0x0b);
+assert.equal(uint8[1], 0x09);
+assert.equal(uint8[2], 0x0a);
+assert.equal(uint8[3], 0x0b);
 
 // test clamped array
 var uint8c = new Uint8ClampedArray(buffer);
