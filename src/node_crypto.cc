e2452810a8d35a8bc3d468c2d9e7acc6cf026a41
Fedor Indutny <fedor.indutny@gmail.com>
2013-12-18 00:33:48 +0400

    ...

--- src/node_crypto.cc
+++ src/node_crypto.cc
@@ -4106,8 +4106,16 @@ void SetEngine(const v8::FunctionCallbackInfo<v8::Value>& args) {
     }
   }
 
-  if (engine == NULL)
-    return ThrowCryptoError(ERR_get_error());
+  if (engine == NULL) {
+    int err = ERR_get_error();
+    if (err == 0) {
+      char tmp[1024];
+      snprintf(tmp, sizeof(tmp), "Engine \"%s\" was not found", *engine_id);
+      return ThrowError(tmp);
+    } else {
+      return ThrowCryptoError(err);
+    }
+  }
 
   int r = ENGINE_set_default(engine, flags);
   ENGINE_free(engine);
