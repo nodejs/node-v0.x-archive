43b37e482e8144268ef4b82836257d09985dc095
Trevor Norris <trev.norris@gmail.com>
2013-12-23 22:48:56 -0800

    events: add observer to individual instance
    Can add an existing observer to an existing event emitter instance.

--- test/simple/test-event-emitter-observer-attach.js
+++ test/simple/test-event-emitter-observer-attach.js
@@ -23,21 +23,16 @@ var common = require('../common');
 var assert = require('assert');
 var events = require('events');
 
-// Check observers are created correctly.
+// Check observer can be added after creation.
 var obs = events.createObserver({
   create: function(context, storage) {
     assert.equal(storage, 'test storage');
   }
 }, 'test storage');
 
-assert.equal(typeof obs.create, 'function');
-assert.equal(obs.flags, 1);
-assert.equal(obs.storage, 'test storage');
-assert.equal(typeof obs.uid, 'number');
-
-events.addObserver(obs);
-
 var ee = new events();
+
+events.attachObserver(ee, obs);
 
 assert.ok(ee._observers);
 assert.equal(ee._observers.length, 1);
@@ -51,51 +46,20 @@ assert.equal(item.storage, 'test storage');
 assert.equal(ee._storage[obs.uid], item.storage);
 
 
-// Check the observer is removed properly.
-events.removeObserver(obs);
+// Check observer can be added after creation and storage changed.
+var obs = events.createObserver({
+  create: function(context, storage) { }
+}, 'test storage');
 
 var ee = new events();
 
-assert.ok(!ee._observers);
-assert.ok(!ee._storage);
-
-
-// Check the storage value can be overridden, and the observer
-// is properly created on addObserver.
-var obs = events.addObserver({
-  create: function(context, storage) {
-    context.testValue = true;
-    return 'this works';
-  }
-}, 'remove me');
-
-var ee = new events();
+events.attachObserver(ee, obs, 'new storage');
 
 assert.ok(ee._observers);
 assert.equal(ee._observers.length, 1);
 assert.equal(ee._observers[0], obs);
-assert.equal(ee._storage[obs.uid], 'this works');
-assert.ok(ee.testValue);
 
 var item = ee._observers[0];
 assert.equal(typeof item.create, 'function');
 assert.equal(item.flags, 1);
-assert.equal(item.storage, 'remove me');
-
-
-// Check the same observer can only be added once.
-events.addObserver(obs);
-
-var ee = new events();
-
-assert.ok(ee._observers);
-assert.equal(ee._observers.length, 1);
-
-// Check the storage value isn't overridden if observer was
-// previously created.
-events.removeObserver(obs);
-events.addObserver(obs, 'not existent');
-
-assert.equal(obs.storage, 'remove me');
-
-events.removeObserver(obs);
+assert.equal(ee._storage[obs.uid], 'new storage');
