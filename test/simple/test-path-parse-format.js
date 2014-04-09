811c93e4f62dd0acaf1513f32ae338ae58611122
Rory Bradford <roryrjb@gmail.com>
2014-04-09 21:37:29 +0100

    path: added windows UNC paths to test.
    also fixed UNC path bug.

--- test/simple/test-path-parse-format.js
+++ test/simple/test-path-parse-format.js
@@ -1,9 +1,36 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 var path = require('../../lib/path');
 
 var winPaths = [
   'C:\\path\\dir\\index.html',
   'C:\\another_path\\DIR\\1\\2\\33\\index',
-  'another_path\\DIR with spaces\\1\\2\\33\\index'
+  'another_path\\DIR with spaces\\1\\2\\33\\index',
+
+  // unc
+  '\\\\server\\share\\file_path',
+  '\\\\server two\\shared folder\\file path.zip',
+  '\\\\teela\\admin$\\system32'
+
 ];
 
 var unixPaths = [
@@ -36,8 +63,10 @@ paths.forEach(function(element, index, array) {
   for (var i = 0; i < Object.keys(output).length; i++) {
     values.push(output[keys[i]]);
   }
-  console.log(count + ': [path.parse()] ' + Object.keys(output) + ' => ' + values);
-  console.log(count + ': [path.format()] `' + path.format(path.parse(element)) + '`');
+  console.log(count + ': [path.parse()] ' + Object.keys(output)
+    + ' => ' + values);
+  console.log(count + ': [path.format()] `'
+    + path.format(path.parse(element)) + '`');
 
   if (
 
