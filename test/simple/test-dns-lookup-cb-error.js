0efab5ae683cf09a3fa31ef01d71a0fb06acf36a
Chris Dickinson <christopher.s.dickinson@gmail.com>
2014-06-12 10:33:48 -0700

    add test for callback asynchronicity

--- test/simple/test-dns-lookup-cb-error.js
+++ test/simple/test-dns-lookup-cb-error.js
@@ -31,7 +31,14 @@ cares.getaddrinfo = function() {
 };
 
 assert.doesNotThrow(function() {
+  var tickValue = 0;
+
   dns.lookup('example.com', function(error, result, addressType) {
+    assert.equal(tickValue, 1);
     assert.equal(error.code, 'ENOENT');
   });
+
+  // Make sure that the error callback is called
+  // on next tick.
+  tickValue = 1;
 });
