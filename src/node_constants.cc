fc3874b12e663b5aab8ca36a8f55fcf521281465
Amim Knabben <amim.knabben@gmail.com>
2014-06-24 10:08:02 -0300

    src: raise _XOPEN_SOURCE definition to 600

--- src/node_constants.cc
+++ src/node_constants.cc
@@ -19,13 +19,13 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// O_NONBLOCK is not exported unless _XOPEN_SOURCE >= 500.
-#if defined(_XOPEN_SOURCE) && _XOPEN_SOURCE < 500
-#undef _XOPEN_SOURCE
+// O_NONBLOCK is not exported unless _XOPEN_SOURCE >= 600.
+#if defined(_XOPEN_SOURCE) && _XOPEN_SOURCE < 600
+# undef _XOPEN_SOURCE
 #endif
 
 #if !defined(_XOPEN_SOURCE)
-#define _XOPEN_SOURCE 500
+# define _XOPEN_SOURCE 600
 #endif
 
 #include "node_constants.h"
