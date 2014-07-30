bc34188de38741c6578a59484920f526f51420bd
Sam Roberts <sam@strongloop.com>
2014-07-29 21:10:56 -0700

    cluster: tests multiple open of ports in workers
    Note that test/simple/test-cluster-dgram-4.js is a simplified case, since
    dgram.send() for clients does an implict .bind(0), so it could also be written
    as:
    
      A = dgram.createSocket('udp4')
      A.send(...)
      A.close();
      ... time passes...
      B = dgram.createSocket('udp4')
      B.send(...)
      B.close();
    
    Above was the original form, dscape/lynx kills the cluster master when run in
    a worker.
    
    IMO, dgram-4 is the most serious of this, using multiple dgram client sockets
    is currently impossible with cluster.
    
    Also note test/simple/test-cluster-net-listen-2.js, the TCP equivalent of
    dgram-4, passes, because TCP close causes the cluster master to be notified,
    UDP close does not.
    
    Also, note that I use the ephemeral "flag" port `0` in the tests, the same
    failure is seen with any port.

---
+++ test/simple/test-cluster-dgram-4.js
@@ -0,0 +1,58 @@
+// Copyright Joyent, Inc. and other Node contributors.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit
+// persons to whom the Software is furnished to do so, subject to the
+// following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN
+// NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
+// USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+var assert = require('assert');
+var cluster = require('cluster');
+var dgram = require('dgram');
+
+if (cluster.isMaster) {
+  // ensure that the worker exits peacefully
+  var worker = cluster.fork();
+  worker.on('exit', function(statusCode) {
+    assert.equal(statusCode, 0);
+    worker = null;
+  });
+  worker.on('message', function(msg) {
+    if (msg === 'PASS') {
+      worker.disconnect();
+    }
+  });
+  process.on('exit', function() {
+    assert.equal(worker, null);
+  });
+
+  return;
+}
+
+// Should open the same ephemeral port twice in a row.
+var A = dgram.createSocket('udp4');
+A.bind(0, function() {
+  A.close();
+
+  var B = dgram.createSocket('udp4');
+  B.bind(0);
+
+  setTimeout(function() {
+    B.close();
+    process.send('PASS');
+  }, 0);
+});
+
