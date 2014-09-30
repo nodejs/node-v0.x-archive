f7b9f1cde29385b870d2949516532a545a84656b
Steven Kabbes <stevenkabbes@gmail.com>
2014-09-30 12:52:40 -0700

    build: .conf -> .h for openssl pre-generated configs

--- deps/openssl/config/opensslconf.h
+++ deps/openssl/config/opensslconf.h
@@ -1,19 +1,19 @@
 #if __APPLE__
     #include "TargetConditionals.h"
     #if TARGET_OS_IPHONE && TARGET_CPU_ARM64
-        #include "opensslconf-ios-arm64.conf"
+        #include "opensslconf-ios-arm64.h"
     #elif TARGET_OS_IPHONE && TARGET_CPU_ARM
-        #include "opensslconf-ios-armv7.conf"
+        #include "opensslconf-ios-armv7.h"
     #elif TARGET_OS_IPHONE && TARGET_CPU_X86
-        #include "opensslconf-ios-i386.conf"
+        #include "opensslconf-ios-i386.h"
     #elif TARGET_OS_IPHONE && TARGET_CPU_X86_64
-        #include "opensslconf-ios-x86_64.conf"
+        #include "opensslconf-ios-x86_64.h"
     #elif TARGET_OS_IPHONE
         #error Unknown iphone platform
     #elif TARGET_OS_MAC && TARGET_CPU_X86
-        #include "opensslconf-mac-i386.conf"
+        #include "opensslconf-mac-i386.h"
     #elif TARGET_OS_MAC && TARGET_CPU_X86_64
-        #include "opensslconf-mac-x86_64.conf"
+        #include "opensslconf-mac-x86_64.h"
     #elif TARGET_OS_MAC
         // Falling back to the generic configs for mac
         #include "opensslconf-generic.h"
