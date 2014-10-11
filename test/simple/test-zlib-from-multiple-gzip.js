4634e30e1bfd8a884e99761ef7eafd1ef3124a48
Luis Reis <luis.m.reis@gmail.com>
2014-10-11 12:24:13 -0700

    zlib: Add require('../common') to multiple gzip stream tests.

--- test/simple/test-zlib-from-multiple-gzip.js
+++ test/simple/test-zlib-from-multiple-gzip.js
@@ -22,6 +22,7 @@
 // test unzipping a file that was created by contactenating multiple gzip
 // streams.
 
+var common = require('../common');
 var assert = require('assert');
 var zlib = require('zlib');
 
