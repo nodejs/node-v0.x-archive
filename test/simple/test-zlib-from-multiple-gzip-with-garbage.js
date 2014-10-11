fb973d35f14825ae77acdc2c168ae72fbfb4fb7f
Luis Reis <luis.m.reis@gmail.com>
2014-10-11 12:32:47 -0700

    zlib: Fix typo in multiple gzip stream test comments.

--- test/simple/test-zlib-from-multiple-gzip-with-garbage.js
+++ test/simple/test-zlib-from-multiple-gzip-with-garbage.js
@@ -19,7 +19,7 @@
 // OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE
 // USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-// test unzipping a file that was created by contactenating multiple gzip
+// test unzipping a file that was created by concatenating multiple gzip
 // streams.
 
 var common = require('../common');
