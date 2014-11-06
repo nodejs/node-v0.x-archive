f98ad99805ad5c822c88c5b56e5355c9721491b7
Cheng Zhao <zcbenz@gmail.com>
2014-11-06 21:31:23 +0800

    src: don't handle UTF16 external string

--- src/string_bytes.cc
+++ src/string_bytes.cc
@@ -277,13 +277,6 @@ bool StringBytes::GetExternalParts(Isolate* isolate,
     *data = ext->data();
     *len = ext->length();
     return true;
-
-  } else if (str->IsExternal()) {
-    const String::ExternalStringResource* ext;
-    ext = str->GetExternalStringResource();
-    *data = reinterpret_cast<const char*>(ext->data());
-    *len = ext->length();
-    return true;
   }
 
   return false;
