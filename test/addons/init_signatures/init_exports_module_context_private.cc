e80f4476aa3e58639ea7591df9d76ebe7b5b9a7e
David Siegel <david.siegel@artcom.de>
2014-12-01 20:31:48 +0100

    Add-on init: Added init signature test cases.

---
+++ test/addons/init_signatures/init_exports_module_context_private.cc
@@ -0,0 +1,8 @@
+#include "init_test.h"
+void init(v8::Handle<v8::Object> exports,
+          v8::Handle<v8::Object> module,
+          v8::Handle<v8::Context> context,
+          void * priv) {
+  NODE_TEST_ADDON_INIT_TAG(exports);
+}
+NODE_MODULE(NODE_TEST_ADDON_NAME, init)
