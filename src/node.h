2c73193417319b20bf386edb48fad99abc5acb61
David Siegel <david.siegel@artcom.de>
2014-12-02 17:23:46 +0100

    Add-on init: relinted... again :-/

--- src/node.h
+++ src/node.h
@@ -388,7 +388,7 @@ struct OptionalInitArg<void*> {
 // signature.
 template <typename F> struct AddonInitAdapter;
 
-#if __cplusplus > 199711L  // C++11  
+#if __cplusplus > 199711L  // C++11
 
 template <typename... Args>
 struct AddonInitAdapter<void (*)(v8::Handle<v8::Object>, Args...)> {
@@ -498,7 +498,7 @@ struct AddonInitAdapter<void (*)(A0, A1, A2, A3)> {
   }
 };
 
-#endif // pre C++11
+#endif  // pre C++11
 
 // utility function to capture the type F
 template <typename F>
