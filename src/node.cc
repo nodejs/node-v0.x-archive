3c20df8633368a0c5f3eb4e22b5691b15c83122d
David Siegel <david.siegel@artcom.de>
2014-12-05 15:11:51 +0100

    Revert "Add-on init: Removed "private" argument"
    This reverts commit dff8b1b13d258404e7934c96133a0ed4ad2e91a8.

--- src/node.cc
+++ src/node.cc
@@ -2047,7 +2047,8 @@ void DLOpen(const FunctionCallbackInfo<Value>& args) {
     mp->nm_register_func(mp->nm_init,
                          exports,
                          module,
-                         env->context());
+                         env->context(),
+                         mp->nm_priv);
   } else {
     env->ThrowError("Module has no declared entry point.");
     return;
@@ -2164,7 +2165,8 @@ static void Binding(const FunctionCallbackInfo<Value>& args) {
     mod->nm_register_func(mod->nm_init,
                           exports,
                           noModule,
-                          env->context());
+                          env->context(),
+                          mod->nm_priv);
     cache->Set(module, exports);
   } else if (!strcmp(*module_v, "constants")) {
     exports = Object::New(env->isolate());
